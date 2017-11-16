a28b414feeca84da21acadf361b82025fc4b17ae
Carlos Reyes Dueñas <king.slave@gmail.com>
2017-11-15 20:21:33 -0600

    Ultima actualización

--- js/gestor.js
+++ js/gestor.js
@@ -1,5 +1,8 @@
 exports.guardarContacto = guardarContacto;
 exports.consultar=consultar;
+exports.eliminar = eliminar;
+exports.obtener = obtener;
+exports.guardarCambios = guardarCambios;
 
 var PouchDB = require('./vendor/pouchdb');
 
@@ -31,9 +34,38 @@ bd.allDocs({include_docs:true,attachments:true,
       "<td>"+doc.rows[i].doc.nombre+"</td>"+
       "<td>"+doc.rows[i].doc.edad+"</td>"+
       "<td>"+doc.rows[i].doc.email+"</td>"+
+      "<td><button class='boton-no' onclick='eliminar(\""+doc.rows[i].doc._id+"\",\""+doc.rows[i].doc._rev+"\")'>Eliminar</button></td>"+
+      "<td><a class='boton' href='modificar.html?id="+doc.rows[i].doc._id+"&rev="+doc.rows[i].doc._rev+"'>Modificar</a></td>"+
       "</tr>";
     }
   }).catch(function(err){
     console.log('ERROR: No se pudo consultar');
   });
+}
+
+function eliminar(id,rev){
+bd.remove(id,rev,function(err,res){
+  if(err){
+    alert('No se pudo eliminar');
+    }
+    else {
+      alert('Documento eliminado!');
+        // window.location="consulta.html";
+        location.reload();
+    }
+});
+}
+
+function obtener(id,mostrar){
+    bd.get(id,mostrar);
+}
+
+function guardarCambios(id,rev,nombre,edad,email,resultado){
+
+bd.put({_id:''+id,
+_rev:''+rev,
+nombre:''+nombre,
+edad:''+edad,
+email:''+email},resultado);
+
 }
